461b8ad54a46c14d89499136bac4c2a98470c060
VladWC <vlad.filin.92@inbox.ru>
2024-09-26 19:15:52 +0300

    hw-02-start

--- javascript.js
+++ javascript.js
@@ -1,1 +1,57 @@
-//start
+// Задание 1
+let a=10;
+alert (a);
+a+=10;
+alert (a);
+// Задание 2
+let releaseDate='2007';
+alert (`Первый iPhone вышел в ${releaseDate} году.`);
+// Задание 3
+let creater='Бренданом Эйхом';
+alert (`Самая первая реализация JavaScript была создана ${creater}.`);
+// Задание 4
+let ten=10;
+let two=2;
+alert (`Сумма: ${ten+two}. Разность: ${ten-two}. Произведение: ${ten*two}. Частное: ${ten/two}.`)
+// Задание 5
+let result=two**5;
+alert(result);
+// Задание 6
+a-=1;
+let b=2;
+alert (a%b);
+// Задание 7
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num++;
+num--;
+alert(num);
+// Задание 8
+let age=prompt('Сколько Вам лет?');
+alert(age);
+// Задание 9
+const user = {
+    name: 'Пользователь',
+    age: 28,
+    isAdmin: true
+ }
+// Задание 10
+let name=prompt('Представтесь, пожалуйста!');
+alert (`Привет, ${name}!`);
+// Задание доп
+let number=Number(prompt('Загадайте любое число!'));
+let result1=number*2;
+alert (result1);
+let result2=result1+10;
+alert (result2);
+let result3=result2/2;
+alert (result3);
+let result4=result3-number;
+alert (result4);
+alert (`Результат равен 5!`);
+
+
+  
